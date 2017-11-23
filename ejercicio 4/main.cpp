1e5741c515ede343147bbdae6ffbd76ea759821d
marconitola01 <33072491+marconitola01@users.noreply.github.com>
2017-11-23 08:54:16 -0500

    Add files via upload

---
+++ ejercicio 4/main.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#define MAX 10
+int main(int argc, char** args)
+{
+int matriz[MAX][MAX], col, reng, i=0, j=0;
+  printf("Cuantas columnas quieres ");
+  scanf("%d", &col);
+  printf("Cuantos renglones quieres ");
+  scanf("%d", &reng);
+  /* LEER LOS DATOS*/
+  for(i = 0;i < reng;i++)
+  {
+      for(j = 0;j < col;j++){
+          printf("ESCRIBE LOS VALORES [%d][%d]=\t", i+1, j+1);
+              scanf("%d", &matriz[i][j]);
+      }
+  }
+  /*RUTINA PARA IMPRIMIR*/
+  printf("\n\n\t\tMatriz original");
+  printf("\n\n");
+  for(i = 0;i < reng;i++){
+      printf("\n\t\t");
+      for(j = 0;j < col;j++){
+          printf("  %6d  ", matriz[i][j]);
+          }
+  }
+ /*IMPRIMIR MATRIZ TRANSPUESTA*/
+  printf("\n\n\t\tMatriz transpuesta");
+  printf("\n\n");
+  for(i = 0;i < col;i++){
+      printf("\n\t\t");
+      for(j = 0;j < reng;j++){
+          printf("  %6d  ", matriz[j][i]);
+          }
+  }
+  printf("\n\n\n");
+  }
+
+

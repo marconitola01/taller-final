b621caf37ec826c56e6755d47b1c76ef9a495ca3
marconitola01 <33072491+marconitola01@users.noreply.github.com>
2017-11-23 08:31:39 -0500

    Update ejercicio.java

--- ejercicio2/src/ejercicio2/ejercicio.java
+++ ejercicio2/src/ejercicio2/ejercicio.java
@@ -5,8 +5,8 @@ public class ejercicio {
      public static void main(String[] ar) {
       // invocaremos los metods para correr el programa
           Matriz5 ma=new Matriz5();
-        ma.cargar();
-        ma.imprimir();
-        ma.matrizEspiral();
+        ma.cargar();//metodo que carga la matriz de numeros 
+        ma.imprimir();//metodo que imprime la matriz original
+        ma.matrizEspiral();//metodo que hace el recorrido en espiral y imprime el recorrido
     }   
 }

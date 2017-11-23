20258c7201e2911e0b01946400d26bc764e123c0
marconitola01 <33072491+marconitola01@users.noreply.github.com>
2017-11-23 08:25:09 -0500

    Update ejercicio.java

--- ejercicio2/src/ejercicio2/ejercicio.java
+++ ejercicio2/src/ejercicio2/ejercicio.java
@@ -3,7 +3,8 @@ package ejercicio2;
 
 public class ejercicio {
      public static void main(String[] ar) {
-        Matriz5 ma=new Matriz5();
+      // invocaremos los metods para correr el programa
+          Matriz5 ma=new Matriz5();
         ma.cargar();
         ma.imprimir();
         ma.matrizcaracol();

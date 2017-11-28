c3ce9ca9dc13870f1489b836ecab29a4b9d4f2ff
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-11-28 11:57:28 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -68,8 +68,8 @@ cout <<"\nlos numeros aleatorios son: \n\n";
 	
 	//imprime los contadores y las listas de los subconjuntos 
 	
-	cout <<"\n\nnumeros pares: " + NumberToString(npar )+ "\n\n";
-	cout <<" numeros impares  son: " + NumberToString(nimpar) + "\n\n";
+	cout <<"\n\n cantidad numeros pares: " + NumberToString(npar )+ "\n\n";
+	cout <<" contidad numeros impares  son: " + NumberToString(nimpar) + "\n\n";
 	cout <<"numeros negativos: " + NumberToString(nneg) + "\n\n";
 	cout <<"numeros positivos: " + NumberToString(npos) + "\n\n";
 	cout <<"numeros pares: "+ cpar + "\n\n";

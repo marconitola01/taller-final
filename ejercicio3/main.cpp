7615c7978474feb7514a706498db889ba26c9a9e
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 17:16:09 -0500

    Update main.cpp

--- ejercicio3/main.cpp
+++ ejercicio3/main.cpp
@@ -3,35 +3,35 @@
 #include <stdlib.h>
 #include <sstream>
 #include <cstdlib>
-#include <ctime>
+#include <ctime>  /*librerias*/
 using namespace std;
 
-// Metodo de conversion int a string 
+// conversion int to string 
 string NumberToString(int pNumber){
  ostringstream oOStrStream;
  oOStrStream << pNumber;
  return oOStrStream.str();
 }
-// main y declaracion de variables
+// main and declaration of variables
 int main(int argc, char* argv[]){
 int m = 0 ;int npar = 0 ;int nimpar = 0;int npos = 0;int nneg = 0 ;int nume;
 string cpar, cimpar, cpos, cneg, x;
 
-//matriz y definir su tamaño
+//matrix and define it's size
 cout << "\n\ningrese las dimenciones de la Matriz: ";
 cin >> m ;
 int matriz [m][m];
 
-//llena matriz con numeros aleatorios 
+//fill matrix with random numbers 
 cout <<"\nlos numeros aleatorios son: \n\n";
 	
 	for (int i= 0 ;i<m ;i++){
-		for (int j= 0 ;j<m ;j++){
-	                nume = 1+rand()%10;
-			matriz[i][j] = nume;
+	for (int j= 0 ;j<m ;j++){
+	         nume = 1+rand()%10;
+	         matriz[i][j] = nume;
 		}
 	}
-// Imprime la matriz	
+// print the matrix	
 	for (int i=0;i<m ;i++){
 		x = "";
 		for (int j= 0 ;j<m ;j++){
@@ -40,40 +40,36 @@ cout <<"\nlos numeros aleatorios son: \n\n";
 		cout << x + "\n";	
 	}
 		
-// define los contadores y subconjuntos de la matriz   
+// subsets   
 	for(int i = 0;i<m;i++){
-		for(int j = 0; j<m;j++){
-			if(matriz[i][j]%2 == 0){
-				//contador y lista de numeros pares
+	for(int j = 0; j<m;j++){
+	if(matriz[i][j]%2 == 0){
+				//pair numbers
 				npar++;
 				cpar = cpar + NumberToString(matriz[i][j]) + ",";
 			}
 			else if (matriz[i][j]%2 != 0){
-				//contador y lista de numeros impares
+				//impair numbers
 				nimpar ++ ;
 				cimpar = cimpar+ NumberToString(matriz[i][j]) + ",";	
 			}
 			if(matriz[i][j]< 0 ){
-				//contador y lista de numeros negativos (-)
+				//negative numbers (-)
 				nneg ++;
 				cneg = cneg + NumberToString(matriz[i][j]) + ",";
 			}
 			else if(matriz[i][j]>= 0){
-				//contador y lista de numeros positivos (+)
+				//positive numbers (+)
 				npos++;
 				cpos = cpos + NumberToString(matriz[i][j]) + ",";			
 			}
-		}
-	}
+		}}
 	
-	//imprime los contadores y las listas de los subconjuntos 
+	//pritn counters and subsets 
 	
-	cout <<"\n\n cantidad numeros pares: " + NumberToString(npar )+ "\n\n";
-	cout <<" contidad numeros impares  son: " + NumberToString(nimpar) + "\n\n";
-	cout <<"numeros negativos: " + NumberToString(nneg) + "\n\n";
-	cout <<"numeros positivos: " + NumberToString(npos) + "\n\n";
-	cout <<"numeros pares: "+ cpar + "\n\n";
-	cout <<"numeros impares:" + cimpar + "\n\n";
+	cout <<"\n\n cantidad numeros pares: " + NumberToString(npar )+ "\n\n"cout <<" contidad numeros impares  son: " + NumberToString(nimpar) + "\n\n";
+	cout <<"numeros negativos: " + NumberToString(nneg) + "\n\n";cout <<"numeros positivos: " + NumberToString(npos) + "\n\n";
+	cout <<"numeros pares: "+ cpar + "\n\n";cout <<"numeros impares:" + cimpar + "\n\n";
 	cout <<"los numeros negativos encontrados son:" + cneg + "\n\n";
 	cout <<"los numeros positivos encontrados son:" + cpos + "\n\n";
 	

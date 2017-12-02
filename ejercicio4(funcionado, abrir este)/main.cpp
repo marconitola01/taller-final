04ca19943506d06bde0c98bd8a95e1b1acaef0c7
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 16:56:13 -0500

    Update main.cpp

--- ejercicio4(funcionado, abrir este)/main.cpp
+++ ejercicio4(funcionado, abrir este)/main.cpp
@@ -3,25 +3,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 #include<stdio.h>
-#define MAX 10  /* el tamaño maximo  de la matriz sera de 10*/
+#define MAX 10  /* The maximum size of the matrix will be 10*/
 
 int main(int argc, char** args)
 {
 int matriz[MAX][MAX], col, reng, i=0, j=0;
-  printf("Cuantas columnas quieres ");         /* pedimos la cantidad de columnas*/
-  scanf("%d", &col);           /*usamos el sacnf y definimos el tipo de dato que recibe %d para decir que va a ser un entero*/
-  printf("Cuantos renglones quieres ");         /* pedimos la cantidad de filas*/
+  printf("Cuantas columnas quieres ");         /* ask the value for "columnas"*/
+  scanf("%d", &col);           /*we use the sacnf and we define the type of data that %d receives to say that it is going to be an intege*/
+  printf("Cuantos renglones quieres ");         /* ask the value for "filas"*/
   scanf("%d", &reng);
   
-  /* esto se encargara de recibir los datos que el usuario ingrese */
-  for(i = 0;i < reng;i++)  /* recorremos la matriz*/
+  /* this will be responsible for receiving the data that the user enters */
+  for(i = 0;i < reng;i++)  /* through the matrix*/
   {
       for(j = 0;j < col;j++){
-          printf("ESCRIBE LOS VALORES [%d][%d]=\t", i+1, j+1);
-              scanf("%d", &matriz[i][j]);
+          printf("ESCRIBE LOS VALORES [%d][%d]=\t", i+1, j+1); /*ask the values of the matrix*/
+              scanf("%d", &matriz[i][j]);    /*receives the elements of the matrix*/
       }
   }
-  /* imprimir matriz*/
+  /* print the matriz*/
   printf("\n\n\t\tMatriz original");
   printf("\n\n");
   for(i = 0;i < reng;i++){
@@ -30,15 +30,15 @@ int matriz[MAX][MAX], col, reng, i=0, j=0;
           printf("  %6d  ", matriz[i][j]);
           }
   }
- /*imprimir la matriz transpuesta*/
+ /*print the transposed matrix */
   printf("\n\n\t\tMatriz transpuesta");
   printf("\n\n");
   for(i = 0;i < col;i++){
       printf("\n\t\t");
       for(j = 0;j < reng;j++){
-          printf("  %6d  ", matriz[j][i]);
+          printf("  %6d  ", matriz[j][i]); /*we invert the rows and columns
           }
   }
   printf("\n\n\n");
-  system("pause");/*para detener el programa*/
+  system("pause");/*stop the program*/
 }

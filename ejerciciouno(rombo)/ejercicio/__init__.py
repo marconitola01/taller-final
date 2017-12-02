8d7f2190a210571672a3b67fe205a92d2ab84d0c
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-02 16:47:35 -0500

    Update __init__.py

--- ejerciciouno(rombo)/ejercicio/__init__.py
+++ ejerciciouno(rombo)/ejercicio/__init__.py
@@ -1,29 +1,29 @@
 import random
 
-matriz = []
+matriz = []      #we create a empty list
 
-filas = int(input("Tamano del arreglo: "))
-columnas = filas
+filas = int(input("Tamano del arreglo: "))  #ask value for size of the array
+columnas = filas   # 
 
-for f in range(0,filas):
-    matriz.append([0]*columnas)
+for f in range(0,filas):   #cycle for 
+    matriz.append([0]*columnas)  # we assing a empty list for each position
     
-for f in range(0,filas):
-    for c in range(0,filas):
-        matriz[f][c] = random.randint(1,9)
-    print()
+for f in range(0,filas):   #cycle for(filas)
+    for c in range(0,filas):   #cycle for(columnas)
+        matriz[f][c] = random.randint(1,9) #we generate ramdom numbers for the array
     
-for f in matriz:
-    for c in f:
-        print(c,end=" ")
+    
+for f in matriz:  #cycle for
+    for c in f:      #cycle for
+        print(c,end=" ")   
     print("")
-print()
-    
-mt = (filas//2)
 
-if filas%2 != 0: 
+    
+mt = (filas//2) #mt mt receives half the matrix to know where to start
+
+if filas%2 != 0:  #the matrix must be square (par o impar)
   
-    for f in range(0,filas):
+    for f in range(0,filas): #par
         for c in range(0,filas):
             if (f+c>=(mt) and f+(mt)>= c and c+(mt)>= f and f+c<=(mt)+filas-1):
                 print(matriz[f][c],end=" ")
@@ -31,7 +31,7 @@ if filas%2 != 0:
                 matriz[f][c] = " "
                 print(matriz[f][c],end=" ")
         print("")              
-else:
+else: #impar
     for f in range(0,filas):
         for c in range(0,filas):
             if (f+c>=(mt)-1 and f+(mt)>= c and c+(mt)>= f and f+c<=(mt)+filas-1):

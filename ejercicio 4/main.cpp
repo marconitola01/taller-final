bd4f6264a835ebe1ea80de344548991ca1aeba5d
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-04 20:10:36 -0500

    Update main.cpp

--- ejercicio 4/main.cpp
+++ ejercicio 4/main.cpp
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#define MAX 10
+#include<stdio.h> /*librerias*/
+#define MAX 10    /*we define the variable max to 10*/
 int main(int argc, char** args)
 {
-int matriz[MAX][MAX], col, reng, i=0, j=0;
-  printf("Cuantas columnas quieres ");
-  scanf("%d", &col);
-  printf("Cuantos renglones quieres ");
-  scanf("%d", &reng);
-  /* LEER LOS DATOS*/
-  for(i = 0;i < reng;i++)
+int matriz[MAX][MAX], col, reng, i=0, j=0; /*variables*/
+  printf("Cuantas columnas quieres ");      /*we ask the value for columns*/
+  scanf("%d", &col);                        /*we define the variable col(columns)*/
+  printf("Cuantos renglones quieres ");     /*we ask the value for reng(renglones)*/
+  scanf("%d", &reng);                       /*we define the variable reng*/
+  /* read the data*/
+  for(i = 0;i < reng;i++)    /*cycle for*/
   {
-      for(j = 0;j < col;j++){
-          printf("ESCRIBE LOS VALORES [%d][%d]=\t", i+1, j+1);
+      for(j = 0;j < col;j++){   /*cycle for*/
+          printf("ESCRIBE LOS VALORES [%d][%d]=\t", i+1, j+1);  /*we ask the value for each position*/
               scanf("%d", &matriz[i][j]);
       }
   }
-  /*RUTINA PARA IMPRIMIR*/
+  /*method for print the original matrix*/
   printf("\n\n\t\tMatriz original");
   printf("\n\n");
   for(i = 0;i < reng;i++){
@@ -24,7 +24,7 @@ int matriz[MAX][MAX], col, reng, i=0, j=0;
           printf("  %6d  ", matriz[i][j]);
           }
   }
- /*IMPRIMIR MATRIZ TRANSPUESTA*/
+ /*print the transpoted matrix*/
   printf("\n\n\t\tMatriz transpuesta");
   printf("\n\n");
   for(i = 0;i < col;i++){

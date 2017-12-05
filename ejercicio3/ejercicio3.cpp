f0215d412393e693c60df9b94f77f9cef0286424
marco nitola <33072491+marconitola01@users.noreply.github.com>
2017-12-05 12:02:01 -0500

    Update ejercicio3.cpp

--- ejercicio3/ejercicio3.cpp
+++ ejercicio3/ejercicio3.cpp
@@ -1,76 +1,139 @@
-#include <iostream>
-#include <string>
 #include <stdlib.h>
-#include <sstream>
-#include <cstdlib>
-#include <ctime>  /*librerias*/
+#include<stdio.h>
+#include <iostream>
+#include<cstdlib>
+#include<ctime>       //librerias
 using namespace std;
 
-// conversion int to string 
-string NumberToString(int pNumber){
- ostringstream oOStrStream;
- oOStrStream << pNumber;
- return oOStrStream.str();
-}
-// main and declaration of variables
-int main(int argc, char* argv[]){
-int m = 0 ;int npar = 0 ;int nimpar = 0;int npos = 0;int nneg = 0 ;int nume; /*variables*/
-string cpar, cimpar, cpos, cneg, x;  
+int main (){   //main for run our program
+	int acuPosi =0, acuNega = 0, acuImpar =0, acuPar =0; //variables
+	srand((int) time(0)); // initialize the numbers random 
+	int m =0;  //m will be the size of matrix
+	printf("ingrese numero: ");//enter number
+	cin>>m;// store in variable m
+	int matriz [m][m];//create matriz bidimensional
+	for (int i =0; i<m; i++){
+		for(int j =0; j<m; j++){
+			matriz [i][j]= (rand() % 15) +-5;  // assign random values 
+		}
+	}
+	cout<<"\n\n";
+	for (int i =0; i<m; i++){ //cycle for
+		cout<<"\n";
+		for(int j =0; j<m; j++){ //cycle for
+			cout<< matriz [i][j];
+			cout<<" ";			// print matriz [][] 	
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){  //cycle for
+		for(int j =0;j<m; j++){  //cycle for
+			if(matriz[i][j] <0 ){ // firt conditional for negatives numbers			
+			acuNega++; // accountant of negatives numbers
+			 cout<<"\n", cout<<"los numeros negativos son: ";
+			cout<< matriz[i][j]; // print matriz with negatives numbers
+			cout<<"\t";
+			}
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] >0 ){// second conditional for positive numbers
+			acuPosi++; // accountant of positive numbers
+            cout<<"los numeros positivos son: ";
+			cout<< matriz[i][j];// print matriz with positives numbers
+			cout<<"\n";
+			
+			}
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] %2== 0 ){ //third conditional for pair numbers
+			acuPar++; // accountant of pair numbers
+			cout<<"los numeros pares son: ";
+			cout<< matriz[i][j]; // print matriz with pair numbers
+			cout<<"\n";
+			}
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] %2!= 0 ){//fourth conditional for odd numbers
+			acuImpar++;//accountant of ood numbers
+			cout<<"los  numeros impares son: ";
+			cout<< matriz[i][j];//print matriz with odd numbers
+			cout<<"\n";
+			}
+		}
+	}// print counters
+	cout<<"\n\t";
+	cout<<"la cantidad de numeros negativos son: ";
+	cout<<"\n\t";
+	cout<< acuNega; //negatives
+	cout<<"\n\t";
+	cout<<"la cantidad de numeros positivos son: ";
+	cout<<"\n\t";
+	cout<< acuPosi;	//positives
+	cout<<"\n\t";
+	cout<<"La cantidad de numeros pares:";
+	cout<< "\n\t";
+	cout<< acuPar; //pairs
+	cout<< "\n\t";
+	cout<< "La cantidad de numeros impares: ";
+	cout<< "\n\t";
+	cout<< acuImpar; //impairs
 
-//matrix and define it's size
-cout << "\n\ningrese las dimenciones de la Matriz: ";   /*we ask value for size*/
-cin >> m ;
-int matriz [m][m];      /*we define the size of the matrix*/
-
-//fill matrix with random numbers 
-cout <<"\nlos numeros aleatorios son: \n\n";   /*generate random numbers*/
 	
-	for (int i= 0 ;i<m ;i++){    /*cycle for*/
-	for (int j= 0 ;j<m ;j++){    /*cycle for*/
-	         nume = 1+rand()%10;     /*we define the range of ramdom numbers*/
-	         matriz[i][j] = nume;      
-		}
-	}
-// print the matrix	
-	for (int i=0;i<m ;i++){
-		x = "";
-		for (int j= 0 ;j<m ;j++){
-			x = x + " " + NumberToString(matriz[i][j])+ " ";
-		}
-		cout << x + "\n";	
-	}
 		
-// subsets   
-	for(int i = 0;i<m;i++){  /*cycle for*/
-	for(int j = 0; j<m;j++){  /*cycle for*/
-	if(matriz[i][j]%2 == 0){   // the number in this position should be pair 
-				//pair numbers
-				npar++;
-				cpar = cpar + NumberToString(matriz[i][j]) + ",";
-			}
-			else if (matriz[i][j]%2 != 0){  //we check's if the number is impair
-				//impair numbers
-				nimpar ++ ;
-				cimpar = cimpar+ NumberToString(matriz[i][j]) + ",";	
-			}
-			if(matriz[i][j]< 0 ){  // for negative numbers, we check's if it's negative
-				//negative numbers
-				nneg ++;
-				cneg = cneg + NumberToString(matriz[i][j]) + ",";
-			}
-			else if(matriz[i][j]>= 0){ // for positive, we check's if it's positive
-				//positive numbers
-				npos++;
-				cpos = cpos + NumberToString(matriz[i][j]) + ","; 			
-			}
-		}}
 	
-	//pritn counters and subsets 
-	
-	cout <<"\n\n cantidad numeros pares: " + NumberToString(npar )+ "\n\n"; cout <<" contidad numeros impares  son: " + NumberToString(nimpar) + "\n\n";
-	cout <<"numeros negativos: " + NumberToString(nneg) + "\n\n";cout <<"numeros positivos: " + NumberToString(npos) + "\n\n";
-	cout <<"numeros pares: "+ cpar + "\n\n";cout <<"numeros impares:" + cimpar + "\n\n";
-	cout <<"los numeros negativos encontrados son:" + cneg + "\n\n";
-	cout <<"los numeros positivos encontrados son:" + cpos + "\n\n";
-	
-}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] >0 ){ //validate, if the number is positive
+			acuPosi++;
+            cout<<"los numeros positivos son: ";
+			cout<< matriz[i][j];
+			cout<<"\n";
+			
+			}
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] %2== 0 ){ //validate, if number is pair
+			acuPar++;
+			cout<<"los numeros pares son: ";
+			cout<< matriz[i][j];
+			cout<<"\n";
+			}
+		}
+	}
+	cout<<"\n\n";
+	for(int i =0; i<m; i++){
+		for(int j =0;j<m; j++){
+			if(matriz[i][j] %2!= 0 ){ //validate, if number is impair
+			acuImpar++;
+			cout<<"los  numeros impares son: ";
+			cout<< matriz[i][j];
+			cout<<"\n";
+			}
+		}
+	}
+	cout<<"\n\t";   // we peint counters and subsets
+	cout<<"la cantidad de numeros negativos son: ";cout<<"\n\t";
+	cout<< acuNega;
+	cout<<"\n\t";cout<<"la cantidad de numeros positivos son: ";
+	cout<<"\n\t";cout<< acuPosi;	
+	cout<<"\n\t";
+	cout<<"La cantidad de numeros pares:";
+	cout<< "\n\t";
+	cout<< acuPar;
+	cout<< "\n\t";
+	cout<< "La cantidad de numeros impares: ";
+	cout<< "\n\t";
+	cout<< acuImpar;
